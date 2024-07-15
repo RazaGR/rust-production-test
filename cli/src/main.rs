b255b9d7c8bc1ebca218be71e4510f364ac83326
Mohsin Raza <mohsin.raza@pensionera.se>
2024-07-15 18:56:31 +0200

    feat: add env and settings

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,5 @@
-mod commands;
 use clap::{Arg, Command};
+use cli::{commands, settings};
 use dotenv::dotenv;
 
 pub fn main() -> anyhow::Result<()> {
@@ -20,6 +20,25 @@ pub fn main() -> anyhow::Result<()> {
 
     let matches = command.get_matches();
 
+    let config_location = matches
+        .get_one::<String>("config")
+        .map(|s| s.as_str())
+        .unwrap_or("");
+
+    let settings = settings::Settings::new(config_location, "CLI")?;
+
+    println!(
+        "db url: {}",
+        settings
+            .database
+            .url
+            .unwrap_or("missing database url".to_string())
+    );
+
+    println!(
+        "log level: {}",
+        settings.logging.log_level.unwrap_or("info".to_string())
+    );
     commands::handle(&matches)?;
 
     Ok(())
