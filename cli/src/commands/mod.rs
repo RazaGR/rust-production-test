5d6a11ab188cb0941365885a75ecb5b77d2d94e4
Mohsin Raza <mohsin.raza@pensionera.se>
2024-07-16 10:58:34 +0200

    feat: add database

--- cli/src/commands/mod.rs
+++ cli/src/commands/mod.rs
@@ -1,4 +1,5 @@
 mod hello;
+mod migrate;
 mod serve;
 
 use clap::{ArgMatches, Command};
@@ -9,11 +10,13 @@ pub fn configure(command: Command) -> Command {
     command
         .subcommand(hello::configure())
         .subcommand(serve::configure())
+        .subcommand(migrate::configure())
 }
 
 pub fn handle(matches: &ArgMatches, settings: &Settings) -> anyhow::Result<()> {
     hello::handle(matches, settings)?;
     serve::handle(matches, settings)?;
+    migrate::handle(matches, settings)?;
 
     Ok(())
 }
